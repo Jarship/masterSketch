838af5f27769b75b429bb443f1d96ff53e15bd7b
John Bishop <JohnArthurBishop@gmail.com>
2015-05-12 13:49:17 -0400

    Changed some ideas with the website.

--- Js/masterSketch.js
+++ Js/masterSketch.js
@@ -1,19 +1,40 @@
-
+var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var conHeight = h * .8;
+var conWidth = w * .6;
 $(document).ready(function(){
 	$("body").append("<div class='container'></div>");
-	for(var x = 0; x < 16; x++)
-	{
-		for(var y = 0; y < 16; y++)
-		{
-			$(".container").append("<div></div>");
-		}
-	}
-	$(".container > div").hover(function(){
-		$(this).addClass("highlight");
+	var initial = 16;
+	create(initial);
+	var divs = $(".container > div");
+	divs.css({"height": "" + conHeight/initial + "px","width": "" + conWidth/initial + "px"});
+	divs.hover(function(){
+		$(this).addClass("highlighta");
 	},function(){
-		$(this).removeClass("highlight");
+		$(this).removeClass("highlighta");
+
+	});
+	divs.click(function(){
+		$(this).toggleClass("selected");
 	});
 
 
-
 });
+
+
+
+function create(z)
+{
+	var count = new Array();
+	var num = 0;
+	for(var x = 0; x < z; x++)
+	{
+		for(var y = 0; y < z; y++)
+		{
+			$(".container").append("<div></div>")
+			/*count[num] = "<div></div>";*/
+			num++;
+		}
+	}
+	/*$(".container").append(count);*/
+}
